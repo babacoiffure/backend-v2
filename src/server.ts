75e5164b67424f8567c8990999a098d5fe3e7c9b
Albi Ummid <albi.ummid@gmail.com>
2025-02-20 20:07:09 +0600

    our server is running meme

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,7 @@ import { connectDB } from "./database";
 import { serverConfigs, serverENV } from "./env-config";
 
 import morgan from "morgan";
+import path from "path";
 import { errorMiddleware, handleNotFound } from "./middleware/error";
 import rootRouter from "./router/http-router";
 import { wsManager } from "./router/socket-manager";
@@ -26,8 +27,15 @@ server.use(
 );
 
 // initializing router
+server.use(rootRouter);
+// Serve static files from the Vite build output
+server.use(express.static(path.join(__dirname, "..", "public")));
 
-server.use(rootRouter, handleNotFound, errorMiddleware);
+// For all other routes, serve index.html (to support client-side routing)
+server.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
+server.use(handleNotFound, errorMiddleware);
 
 // initializing socket events
 wsManager.initialize(socketServer);
