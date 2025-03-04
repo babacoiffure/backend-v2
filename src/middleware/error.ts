185a2d817ab0d7e2f0eb5f37b4442f8959450070
Albi Ummid <albi.ummid@gmail.com>
2025-03-04 23:30:26 +0600

    default 400 status for custom error

--- src/middleware/error.ts
+++ src/middleware/error.ts
@@ -4,7 +4,7 @@ import { serverENV } from "../env-config";
 // Error Handler class
 export class ErrorHandler extends Error {
     statusCode: number;
-    constructor(message: string, statusCode: number) {
+    constructor(message: string, statusCode: number = 400) {
         super(message);
         this.statusCode = statusCode;
         Error.captureStackTrace(this, this.constructor);
